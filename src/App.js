828383f21582f63f8a204e4190be2f928ea0145f
binyoma <innocentbinyoma@pm.me>
2022-09-28 10:54:39 +0200

    add  dynamically categories and filter by them

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
-
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 function App() {
-  const [menuItems, setMenuItems]=useState(items);
-  const [menuCategories, setMenuCategories]=useState([]);
-  return <main>
-    <section className='menu section'>
-      <div className='title'>
-         <h2>our menu</h2>
-         <div className='underline'></div>
-      </div>
-      <Categories/>
-      <Menu items={menuItems}/>
-    </section>
-  </main>;
+  const [menuItems, setMenuItems] = useState(items)
+  const [menuCategories, setMenuCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newitems = items.filter((item) => item.category === category)
+    setMenuItems(newitems)
+  }
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={menuCategories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
 }
 
-export default App;
-
+export default App
